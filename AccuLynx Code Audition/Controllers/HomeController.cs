8123c7220bef19572b29211aef41260f947a190d
Andre <Andre@DESKTOP-UAUALNF>
2023-08-17 11:15:52 -0500

    Add project files.

---
+++ AccuLynx Code Audition/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using AccuLynx_Code_Audition.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AccuLynx_Code_Audition.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ AccuLynx Code Audition/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AccuLynx_Code_Audition.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
